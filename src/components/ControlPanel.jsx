7dc9e8675e4402301953e56d80acba3b16a0eb9f
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2021-02-03 21:07:56 -0500

    feat(components): Create ControlPanel

---
+++ src/components/ControlPanel.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  controlPanel: {
+    paddingTop: theme.spacing(10),
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function ControlPanel() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.controlPanel}>
+      <Button variant="contained" color="primary">
+        Load DB
+      </Button>
+      <Button variant="contained" color="primary">
+        Query DB
+      </Button>
+      <Button variant="contained" color="secondary">
+        Clear DB
+      </Button>
+    </div>
+  );
+}
