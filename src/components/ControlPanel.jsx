2980107b96aaa5c6bb49eebf066b68c435983b24
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2021-02-09 10:11:24 -0500

    feat(ControlPanel): Connect LoadDB button with loadDB event handler

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
+import { loadDB } from "../actions";
+
 const useStyles = makeStyles((theme) => ({
   controlPanel: {
     paddingTop: theme.spacing(10),
@@ -16,7 +18,7 @@ export default function ControlPanel() {
 
   return (
     <div className={classes.controlPanel}>
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" onClick={() => loadDB()}>
         Load DB
       </Button>
       <Button variant="contained" color="primary">
