861165bf6d4c41eb7597ba3ae7f71d2515a9e0da
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2021-02-09 10:06:46 -0500

    feat(store): Add customers initial state

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+const store = {
+  customers: [
+    { name: 'Daniela', email: 'dani@laboratoria.la' },
+    { name: 'Maia', email: 'maia@laboratoria.la' },
+    { name: 'Mery', email: 'mery@laboratoria.la' },
+    { name: 'Paula', email: 'paula@laboratoria.la' },
+    { name: 'Steph', email: 'steph@laboratoria.la' }
+  ]
+};
+
+export default store;
