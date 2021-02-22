6cf380da80c216beff88f043dbbb57c835a35676
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2021-02-22 10:54:17 -0500

    feat(containers): Create Home view

---
+++ src/containers/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ControlPanel from '../components/ControlPanel';
+
+const Home = () => (
+  <div className="Home">
+    <section className="Home-leftcolumn">
+      <ControlPanel />
+    </section>
+  </div>
+);
+
+export default Home;
