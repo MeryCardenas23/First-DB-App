071cf71024569be78d0080765604c255f161be7d
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2021-02-26 08:49:43 -0500

    feat(Home): Add ResultArea

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
 import ControlPanel from '../components/ControlPanel';
+import ResultArea from '../components/ResultArea';
+import { loadDB, getCustomers } from "../actions";
+
+const showCustomers = (customers) => {
+  const resultArea = document.getElementById('ResultArea');
+
+  const title = document.createElement('H2');
+  title.textContent = 'Customer List';
+
+  const list = document.createElement('OL');
+
+  customers.map(customer => {
+    const item = document.createElement('LI');
+    item.textContent = customer.name;
+
+    return list.appendChild(item);
+  });
+
+  resultArea.appendChild(title);
+  resultArea.appendChild(list);
+
+  return resultArea;
+}
 
 const Home = () => (
   <div className="Home">
+    <section className="Home-rightcolumn">
+      <ResultArea />
+    </section>
     <section className="Home-leftcolumn">
-      <ControlPanel />
+      <ControlPanel
+        actions={{
+          loadDB,
+          getCustomers,
+          showCustomers,
+        }}
+      />
     </section>
   </div>
 );
