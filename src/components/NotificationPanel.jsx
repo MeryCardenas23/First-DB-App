685033105f5eac8810da9e5624fd81eaa057a73b
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2021-02-26 07:20:59 -0500

    feat(components): Create NotificationPanel

---
+++ src/components/NotificationPanel.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { Paper, Typography } from '@material-ui/core';
+
+const styles = (theme) => ({
+  root: {
+    minHeight: 500,
+    minWidth: 275,
+    padding: theme.spacing(2),
+  },
+});
+
+const NotificationPanel = (props) => {
+  const { classes } = props;
+
+  return (
+    <>
+      <Typography variant="h4" gutterBottom>
+        Notifications
+      </Typography>
+
+      <Paper id="Notifications" className={classes.root} elevation={3} />
+    </>
+  )
+}
+
+NotificationPanel.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(NotificationPanel);
