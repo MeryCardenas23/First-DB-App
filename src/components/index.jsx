99720d8e0c3a1a4f910d5a4423740b9ca7c392b7
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2021-02-26 08:16:34 -0500

    refactor(components): Add index to export components

---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+export { default as Header } from './Header';
+export { default as ControlPanel } from './ControlPanel';
+export { default as ResultArea } from './ResultArea';
+export { default as NotificationPanel } from './NotificationPanel';
