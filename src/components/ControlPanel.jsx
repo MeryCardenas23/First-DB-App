efafcdb27cb2d0d08ee1416786fe625c8578a80f
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2021-02-26 08:26:24 -0500

    refactor(ControlPanel): Update layout

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import { Button, Typography } from '@material-ui/core';
 
 const styles = (theme) => ({
   root: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    minHeight: 500,
+    minWidth: 275,
     '& > *': {
+      height: 50,
       margin: theme.spacing(1),
+      width: 150,
     },
   },
 });
@@ -16,17 +24,23 @@ const ControlPanel = (props) => {
   const { loadDB, getCustomers, showCustomers, clearDB } = actions;
 
   return (
-    <div className={classes.root}>
-      <Button variant="contained" color="primary" onClick={() => loadDB()}>
-        Load DB
-      </Button>
-      <Button variant="contained" color="primary" onClick={() => getCustomers(showCustomers)}>
-        Query DB
-      </Button>
-      <Button variant="contained" color="secondary" onClick={() => clearDB()}>
-        Clear DB
-      </Button>
-    </div>
+    <>
+      <Typography variant="h4" gutterBottom>
+        Controls
+      </Typography>
+
+      <div className={classes.root}>
+        <Button variant="contained" color="primary" onClick={() => loadDB()}>
+          Load DB
+        </Button>
+        <Button variant="contained" color="primary" onClick={() => getCustomers(showCustomers)}>
+          Query DB
+        </Button>
+        <Button variant="contained" color="secondary" onClick={() => clearDB()}>
+          Clear DB
+        </Button>
+      </div>
+    </>
   );
 }
 
